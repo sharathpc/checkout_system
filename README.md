a3e8d44040c53c173029481a6f742a9f6871b688
sharathpc <sharathchandra0033@gmail.com>
2022-10-05 20:51:40 +0530

    Basic TestCases completed with ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Checkout System
+
+**Demo**: [https://sharathpc.github.io/checkout_system](https://sharathpc.github.io/checkout_system)
+
+## Tech Stack and Time Spent
+- Frontend: Create React App(Typescript)
+  - Time Spent: 2 hours
+- Testing: React Testing Libiary
+  - Time Spent: 30 minutes 
+## Install Dependencies and Run app(Development)
+
+```bash
+$ yarn install
+$ yarn start
+```
+
+ Navigate to `http://localhost:3000/`.
+
+## Build app(Production)
+
+```bash
+$ yarn build
+```
+The build artifacts will be stored in the `build/` directory.
+
+
+## Key decisions
+
+1. Used create react app to bootstrap app quickly
+2. To make the app simple, I have used only App component and also default data
+3. Embedded offer details at each product level to calculate discount price
+4. Used Bootstrap CSS and SCSS to make styling easy
+
+## Future Improvements
+
+1. Create seperate components for easy maintenance and testing
+2. Decouple data from component and load it from API/Service
+3. Write generic function to handle both add and delete functionality 
+4. Build more appealing UX/UI
+5. Refactor the project to be highly scalable and add more e2e testcases
--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,71 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Renders React App', () => {
+it('Renders React App', () => {
   render(<App />);
-  const titleEl = screen.getByRole('heading', { level: 1 });
-  expect(titleEl).toBeInTheDocument();
-  expect(titleEl).toHaveTextContent('Checkout System');
+  const headingElement = screen.getByRole('heading', {
+    name: 'Checkout System'
+  });
+  expect(headingElement).toBeInTheDocument();
+});
+
+it('Validate Products List', () => {
+  render(<App />);
+  const productsList = screen.getAllByRole('listitem');
+  expect(productsList.length).toBe(4);
+});
+
+it('Validate Empty Basket', () => {
+  render(<App />);
+  const emptyBasketElement = screen.getByText('Basket Empty');
+  expect(emptyBasketElement).toBeInTheDocument();
+});
+
+it('Add and Remove Product to Basket', async () => {
+  render(<App />);
+  const bProductElement = screen.getAllByRole('listitem')[1];
+  const addButton = bProductElement.querySelector('.btn-outline-primary');
+  const removeButton = bProductElement.querySelector('.btn-outline-secondary');
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  const productsList = document.querySelectorAll('tbody>tr');
+  expect(productsList.length).toBe(1);
+
+  await act(() => {
+    removeButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  const newProductsList = document.querySelectorAll('tbody>tr');
+  expect(newProductsList.length).toBe(0);
+});
+
+it('Verify Product Quantity in Basket', async () => {
+  render(<App />);
+  const bProductElement = screen.getAllByRole('listitem')[1];
+  const addButton = bProductElement.querySelector('.btn-outline-primary');
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  const productQuantity = document.querySelector('tbody>tr>td:nth-child(4)');
+  expect(productQuantity?.textContent).toBe('2');
+});
+
+it('Verify Product Discount in Basket', async () => {
+  render(<App />);
+  const bProductElement = screen.getAllByRole('listitem')[0];
+  const addButton = bProductElement.querySelector('.btn-outline-primary');
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  await act(() => {
+    addButton?.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  const productQuantity = document.querySelector('tbody>tr>td:nth-child(6)');
+  expect(productQuantity?.textContent).toBe('£1.30');
 });
